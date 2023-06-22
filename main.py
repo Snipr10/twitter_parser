0e54b1d6971542c202c42ff68b5db34bb50b93cd
oleg.gusev <oleg.gusev@gmail.com>
2023-06-22 17:00:18 +0300

    models

--- main.py
+++ main.py
@@ -85,16 +85,8 @@ if __name__ == '__main__':
 
     futures = []
 
-    from twitter_parser.tasks import start_parsing_by_keyword
+    from twitter_parser.tasks import start_parsing_by_keyword, start_parsing_account_source
 
-    #
-    # x = threading.Thread(target=update_, args=(0,))
-    # x.start()
-
-
-    # start_parsing_by_source_while(False)
-    x = threading.Thread(target=new_process_account_item, args=(0, ))
-    x.start()
     #
     # for i in range(1):
     #     time.sleep(10)
@@ -113,103 +105,4 @@ if __name__ == '__main__':
         print("thread new_process_key " + str(i))
         x = threading.Thread(target=new_process_key, args=(i, False,))
         x.start()
-
-    # for i in range(2):
-    #     time.sleep(10)
-    #     print("thread new_process_key " + str(i))
-    #     x = threading.Thread(target=new_process_key, args=(i, True,))
-    #     x.start()
-
-    # i = 1
-    # while True:
-    #     i += 1
-    #     time.sleep(180)
-    #     try:
-    #         django.db.close_old_connections()
-    #         try:
-    #             Account.objects.filter(
-    #                                    last_parsing__lte=update_time_timezone(
-    #                                        timezone.now() - datetime.timedelta(minutes=60)),
-    #                                    ).update(taken=0, banned=0)
-    #         except Exception as e:
-    #             print(e)
-    #         try:
-    #             if i % 10 == 0:
-    #                 try:
-    #                     Keyword.objects.filter(network_id=network_id, enabled=1, taken=1).update(taken=0)
-    #                 except Exception as e:
-    #                     print(e)
-    #                 try:
-    #                     SourcesItems.objects.filter(network_id=network_id, taken=1).update(taken=0)
-    #                 except Exception as e:
-    #                     print(e)
-    #                 try:
-    #                     SourcesItems.objects.filter(network_id=3, disabled=0, last_modified__isnull=True).update(
-    #                         last_modified=datetime.datetime(2000, 1, 1))
-    #                     SourcesItems.objects.filter(network_id=3, disabled=0,
-    #                                                 last_modified__lte=datetime.datetime(1999, 1, 1)).update(
-    #                         last_modified=datetime.datetime(2000, 1, 1))
-    #                 except Exception as e:
-    #                     print(e)
-    #                 i = 0
-    #         except Exception as e:
-    #             print(e)
-    #         try:
-    #             select_sources = Sources.objects.filter(status=0)
-    #             sources_item = SourcesItems.objects.filter(network_id=network_id, disabled=0, taken=0,
-    #                                                        last_modified__isnull=False,
-    #                                                        source_id__in=list(
-    #                                                            select_sources.values_list('id', flat=True))
-    #                                                        )
-    #             sources_item.update(disabled=1)
-    #         except Exception as e:
-    #             print(e)
-    #         try:
-    #             accounts = Account.objects.filter(
-    #                 last_parsing__lte=update_time_timezone(
-    #                     timezone.now() - datetime.timedelta(minutes=60)),
-    #             )
-    #             proxies_set = set()
-    #             for a in accounts:
-    #                 proxies_set.add(a.proxy_id)
-    #             proxies_list = list(proxies_set)
-    #             print(f"proxies_list {proxies_list}")
-    #             work_proxy = []
-    #             work_proxy_ids = []
-    #             proxy_candidates = AllProxy.objects.filter(port__in=[30001, 30011, 30010])
-    #             for can in proxy_candidates:
-    #                 try:
-    #                     proxy_str = f"{can.login}:{can.proxy_password}@{can.ip}:{can.port}"
-    #                     proxies = {'https': f'http://{proxy_str}'}
-    #                     if requests.get("https://www.facebook.com/", proxies=proxies, timeout=10).ok:
-    #                         work_proxy.append(can)
-    #                         work_proxy_ids.append(can.id)
-    #
-    #                 except Exception as e:
-    #                     print(f"{proxy_str} {e}")
-    #             print(f"work_proxy {work_proxy}")
-    #             print(f"work_proxy_ids {work_proxy_ids}")
-    #
-    #             exist_proxy = AllProxy.objects.filter(id__in=proxies_list)
-    #             for e_p in exist_proxy:
-    #                 try:
-    #                     proxy_str = f"{e_p.login}:{e_p.proxy_password}@{e_p.ip}:{e_p.port}"
-    #                     proxies = {'https': f'http://{proxy_str}'}
-    #                     if requests.get("https://www.facebook.com/", proxies=proxies, timeout=10).ok:
-    #                         work_proxy.append(e_p)
-    #                         work_proxy_ids.append(e_p.id)
-    #                 except Exception as e:
-    #                     print(f"{proxy_str} {e}")
-    #             print(f"work_proxy {work_proxy}")
-    #             print(f"work_proxy_ids {work_proxy_ids}")
-    #             for a in accounts:
-    #                 if a.proxy_id not in work_proxy_ids:
-    #                     a.proxy_id = random.choice(work_proxy_ids)
-    #                     a.save(update_fields=["proxy_id"])
-    #                     print(f"account {a}")
-    #         except Exception as e:
-    #             print(e)
-    #     except Exception as e:
-    #         print(e)
-
 
